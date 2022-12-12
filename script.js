638a6ba3078bc5b815001845ce88a35104a0cc6d
Harry Watkins <harry.jw@outlook.com>
2022-12-12 14:39:01 +0000

    Add board and gameBoard, displayController methods which can display the game board

--- script.js
+++ script.js
@@ -2,4 +2,34 @@
 
     "use strict"
 
+    const gameBoard = (() => {
+
+        let board = [
+            ["X","O","X"],
+            ["X","O","X"],
+            ["X","O","X"]
+        ];
+
+        const getBoard = () => {
+            return board
+        }
+
+        const getSquare = (i) => {
+            return board[i]
+        }
+
+    })();
+
+    const displayController = (() => {
+
+        const squares = document.querySelectorAll(".square");
+
+        const updateBoard = () => {
+            for (let i = 0; i < squares.length; i++) {
+                squares[i].textContent = gameBoard.getSquare(i);
+            }
+        }
+
+    })();
+
 })()
